03e29213d50f00fa2072ae348ac221a91d990118
ashafaatadhis <skullreaper387@gmail.com>
2025-08-11 08:25:35 +0700

    feat: generate kwitansi

--- services/purchase_service.go
+++ services/purchase_service.go
@@ -110,7 +110,7 @@ func (s *PurchaseService) generateAndSendReceipt(purchase *models.Purchase) erro
 		return fmt.Errorf("gagal buat folder temp: %w", err)
 	}
 
-	fmt.Println(tempDir, "www333")
+	fmt.Println(tempDir, "www334")
 	// Pastikan folder dan isinya dihapus setelah selesai
 	defer os.RemoveAll(uniqueFolder)
 
